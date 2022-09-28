2c5253207bf00c47bcd1a335eb2fc1d6c829785c
EPAM\Lizhu_Wang <taur57@hotmail.com>
2022-09-28 16:40:22 +0800

    Updates

---
+++ precache-manifest.0b10d043152e174909447c412a93f522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f6be13ad333fb4ac8b6",
+    "url": "/st/static/css/main.3dbc4f83.chunk.css"
+  },
+  {
+    "revision": "8f6be13ad333fb4ac8b6",
+    "url": "/st/static/js/main.8f6be13a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/st/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/st/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "0415b752cab1029dfc8b",
+    "url": "/st/static/js/runtime~main.0415b752.js"
+  },
+  {
+    "revision": "20a3745d56f8ca8c80b86f32d3f08e19",
+    "url": "/st/index.html"
+  }
+];
